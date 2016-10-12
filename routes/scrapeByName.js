aa77b03c3e54da0f41afb3e6eb6facb2eeccabd0
William Elimbi <williamelimbi@gmail.com>
2016-10-12 22:28:51 +0200

    serialze request

--- routes/scrapeByName.js
+++ routes/scrapeByName.js
@@ -4,15 +4,24 @@ var request = require('request');
 //var formData = require('form-data');
 var fs = require('fs');
 var cheerio = require('cheerio');
+var async = require('async');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     let arrayForm = [];
+    let arr = [];
     let id = 30000;
     let it = 0;
-    let form = {
-        a: 'recherche_ligne',
-        ligne_sens: '405_A'
+
+    for (let i = 401; i < 421; i++) {
+        arrayForm.push({
+            a: 'recherche_ligne',
+            ligne_sens: i + '_A'
+        })
+        arrayForm.push({
+            a: 'recherche_ligne',
+            ligne_sens: i + '_B'
+        })
     }
     const goSearch = formObj => {
         const finalUrl = 'http://www.bus-tice.com/se-deplacer/timeo-vos-horaires-en-temps-reel/';
@@ -29,33 +38,42 @@ router.get('/', function(req, res, next) {
                 ligne: input[4].attribs.value,
                 code: input[2].attribs.value
             }
-            let arr = [];
+
             let i = 0;
-            for (key in $('#list_refs option')){
-                if ( i > 0) {
-                  let station =$('#list_refs option')[key].hasOwnProperty('children')? $('#list_refs option')[key].children[0].data.trim().split(""): "";
-                  if(station.length !== 0) {
-                    station.shift();
-                    station.shift();
-                    station.splice(-6);
-                    station = station.join('');
-                    arr.push(Object.assign({}, form, {
+            for (key in $('#list_refs option')) {
+                if (i > 0) {
+                    let station = $('#list_refs option')[key].hasOwnProperty('children')
+                        ? $('#list_refs option')[key].children[0].data.trim().split("")
+                        : "";
+                    if (station.length !== 0) {
+                        station.shift();
+                        station.shift();
+                        station.splice(-6);
+                        station = station.join('');
+                        arr.push(Object.assign({}, formObj, {
 
-                        refs: $('#list_refs option')[key].attribs.value,
-                        station: station.trim()
-                    }))
-                  }
+                            refs: $('#list_refs option')[key].attribs.value,
+                            station: station.trim()
+                        }))
+                        console.log(arr[i]);
+                        fs.writeFile('output1.json', JSON.stringify(Object.assign({}, formObj, {
 
+                            refs: $('#list_refs option')[key].attribs.value,
+                            station: station.trim()
+                        })))
+                    }
 
-              }
-              i++;
+                }
+                i++;
             }
-
-            res.send(html);
+            //res.send(html);
         })
     }
-  
-    goSearch(form);
+    async.eachSeries(arrayForm, function iteratee(item, callback) {
+        goSearch(item);
+        callback(false);
+    })
+    //
 
 })
 
