9777a0b0fac0869c5e3d086e76ca87499240255c
William Elimbi <williamelimbi@gmail.com>
2016-10-19 00:58:25 +0200

    add img for each lines , request on server

--- tice-mobile/src/pages/search/search.ts
+++ tice-mobile/src/pages/search/search.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { Http,Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
 import { NavController } from 'ionic-angular';
 import {StationCode} from '../../service/stationCode';
 @Component({
@@ -10,22 +11,45 @@ export class SearchPage {
   public placeholder = 'Votre recherche';
   private array;
   public results;
-  constructor(public navCtrl: NavController, public stationCode: StationCode) {
+  constructor(public navCtrl: NavController, public stationCode: StationCode, public http: Http) {
     console.log('hello dans search');
-    this.array = stationCode.data;
+    this.array = stationCode.data.map((item, index, arr) => {
+      for (let i = 0; i < arr.length - 1; i++) {
+        if (item.station === arr[i].station && item.ligne === arr[i].ligne && item.code) {
+          return {
+            station: item.station,
+            ligne: item.ligne,
+            codes: [item.code, arr[i].code]
+          }
+        }
+      }
+      //console.log(this.array);
+    });
   }
-  onInput(event) {
-    let val = event.target.value.trim();
-    if (val.length > 0) {
-      this.results = this.array.filter(item => {
-        return item.station.toLowerCase().indexOf(event.target.value) > -1;
-      })
-    } else {
-      this.results = [];
-    }
+  onClick(item) {
 
+    this.http.post('http://www.bus-tice.com/se-deplacer/timeo-vos-horaires-en-temps-reel/',{a:'recherche_code', code:item.codes[1]}).map(res => res.json()).subscribe(data => {
+        console.log(data);
+      });
   }
-  onCancel(event) {
-    console.log(event.target.value);
+
+onInput(event) {
+  let val = event.target.value.trim();
+  if (val.length > 0) {
+    this.results = this.array.filter(item => {
+      return item.station.toLowerCase().indexOf(event.target.value) > -1 || val === item.ligne;
+    })
+
+  } else {
+    this.results = [];
   }
+
+  // if (this.results.length > 0) {
+  //   console.log(this.results);
+  // }
+
+}
+onCancel(event) {
+  console.log(event.target.value);
+}
 }
